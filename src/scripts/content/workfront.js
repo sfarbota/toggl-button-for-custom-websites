f90596e529fac1fe2fc5cb5f7fdeb888b49dd45e
Dan Diemer <ddiemer@americanbible.org>
2015-10-22 16:23:05 -0400

    Remove dependancy on jQuery

--- src/scripts/content/workfront.js
+++ src/scripts/content/workfront.js
@@ -5,16 +5,15 @@
 
 togglbutton.render('.work-item-container:not(.toggl)', {observe: true}, function (elem) {
   var link, seperator = createTag('span','','|'),
-    container = $('.work-item-container .work-item-middle .extras .user-actions', elem),
-    titleElem = $('.work-item-container .work-item-middle .names .item-name a span', elem),
-    projectElem = $('.work-item-container .work-item-middle .names .project-name a span', elem);
-
+    container = elem.querySelectorAll('.work-item-container .work-item-middle .extras .user-actions'),
+    titleElem = elem.querySelectorAll('.work-item-container .work-item-middle .names .item-name a span'),
+    projectElem = elem.querySelectorAll('.work-item-container .work-item-middle .names .project-name a span');
   link = togglbutton.createTimerLink({
     className: 'workfront',
-    description: titleElem.innerHTML,
-    projectName: projectElem.textContent
+    description: titleElem[0].innerHTML,
+    projectName: projectElem.length ? projectElem[0].textContent : ""
   });
 
-  container.appendChild(seperator);
-  container.appendChild(link);
+  container[0].appendChild(seperator);
+  container[0].appendChild(link);
 });
