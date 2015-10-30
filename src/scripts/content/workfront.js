584d694a102f39274aff0e2ba05ae09c69f77925
Dan Diemer <ddiemer@americanbible.org>
2015-10-30 14:21:45 -0400

    make workfront a little less error-y

--- src/scripts/content/workfront.js
+++ src/scripts/content/workfront.js
@@ -3,17 +3,40 @@
 
 'use strict';
 
-togglbutton.render('.work-item-container:not(.toggl)', {observe: true}, function (elem) {
-  var link, seperator = createTag('span','','|'),
-    container = elem.querySelectorAll('.work-item-container .work-item-middle .extras .user-actions'),
-    titleElem = elem.querySelectorAll('.work-item-container .work-item-middle .names .item-name a span'),
-    projectElem = elem.querySelectorAll('.work-item-container .work-item-middle .names .project-name a span');
-  link = togglbutton.createTimerLink({
-    className: 'workfront',
-    description: titleElem[0].innerHTML,
-    projectName: projectElem.length ? projectElem[0].textContent : ""
+//My Work - Working on view
+if(document.querySelector('#mywork') !== null) {
+  togglbutton.render('.work-item-container:not(.toggl)', {observe: true}, function (elem) {
+    var link, seperator = createTag('span','','|'),
+      container = elem.querySelectorAll('.work-item-container .work-item-middle .extras .user-actions'),
+      titleElem = elem.querySelectorAll('.work-item-container .work-item-middle .names .item-name a span'),
+      projectElem = elem.querySelectorAll('.work-item-container .work-item-middle .names .project-name a span');
+    link = togglbutton.createTimerLink({
+      className: 'workfront',
+      description: titleElem[0].innerHTML,
+      projectName: projectElem.length ? projectElem[0].textContent : ""
+    });
+
+    container[0].appendChild(seperator);
+    container[0].appendChild(link);
+  });
+}
+//Issue inner view
+if(document.querySelector('.DetailsView') !== null) {
+
+  togglbutton.render('#layout-content:not(.toggl)', {observe: true}, function (elem) {
+    var link, seperator = createTag('span','','|'),
+      container = elem.querySelectorAll('.detail-tabs #UpdatesToolbar'),
+      titleElem = elem.querySelectorAll('.DetailsView .pos-r .details-header h1 div'),
+      projectElem = elem.querySelectorAll('.DetailsView .tile-loader a:first-of-type');
+      console.log(titleElem[0]);
+    link = togglbutton.createTimerLink({
+      className: 'workfront',
+      description: titleElem[0].innerHTML,
+      projectName: projectElem.length ? projectElem[0].textContent : ""
+    });
+
+    container[0].appendChild(seperator);
+    container[0].appendChild(link);
   });
 
-  container[0].appendChild(seperator);
-  container[0].appendChild(link);
-});
+}
